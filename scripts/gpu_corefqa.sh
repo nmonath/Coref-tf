eed7f721951ced0d41e71990d42116b035979709
xiaoyli@outlook.com <xiaoyli@outlook.com>
2020-07-04 17:20:05 +0800

    fix

---
+++ scripts/gpu_corefqa.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash 
+# -*- coding: utf-8 -*- 
+
+
+
+# author: xiaoy li 
+# description:
+# 
+
+
+REPO_PATH=/home/lixiaoya/xiaoy_tf
+export PYTHONPATH=${REPO_PATH}
+
+OUTPUT_DIR=/xiaoya/export_dir
+
+
+CUDA_VISIBLE_DEVICES=0 python3 ${REPO_PATH}/run/train_corefqa.py \
+--output_dir=${OUTPUT_DIR} \
+--do_train=True \
+--use_tpu=False \
+--iterations_per_loop=500
