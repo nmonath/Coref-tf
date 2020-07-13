0843179e771757e2111051acd2c9c2eecee8cd81
xiaoyli@outlook.com <xiaoyli@outlook.com>
2020-07-13 21:17:27 +0800

    forbid train-loss-hook on TPU; because the loss-hook introduces variable missing on tpu

--- run/train_mention_proposal.py
+++ run/train_mention_proposal.py
@@ -91,12 +91,17 @@ def model_fn_builder(config):
                 optimizer = tf.train.AdamOptimizer(learning_rate=config['learning_rate'], beta1=0.9, beta2=0.999, epsilon=1e-08)
                 optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
                 train_op = optimizer.minimize(total_loss, tf.train.get_global_step()) 
+                output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+                    mode=mode,
+                    loss=total_loss,
+                    train_op=train_op,
+                    scaffold_fn=scaffold_fn)
             else:
                 optimizer = RAdam(learning_rate=config['learning_rate'], epsilon=1e-8, beta1=0.9, beta2=0.999)
                 train_op = optimizer.minimize(total_loss, tf.train.get_global_step())
         
-            train_logging_hook = tf.train.LoggingTensorHook({"loss": total_loss}, every_n_iter=1)
-            output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+                train_logging_hook = tf.train.LoggingTensorHook({"loss": total_loss}, every_n_iter=1)
+                output_spec = tf.contrib.tpu.TPUEstimatorSpec(
                     mode=mode,
                     loss=total_loss,
                     train_op=train_op,
