353c04566444588b6aa65b4280598ab13424003f
xiaoyli@outlook.com <xiaoyli@outlook.com>
2020-07-20 16:23:00 +0800

    update scripts for data generation

--- scripts/data/transform_ckpt_pytorch_to_tf.sh
+++ scripts/data/transform_ckpt_pytorch_to_tf.sh
@@ -10,24 +10,32 @@
 
 
 
-REPO_PATH=/home/lixiaoya/xiaoy_tf
+REPO_PATH=/home/lixiaoya/coref-tf
 export PYTHONPATH=${REPO_PATH}
 
 
-# for spanbert large 
-echo "Transform SpanBERT Cased Base from Pytorch To TF"
-python3 ${REPO_PATH}/run/transform_spanbert_pytorch_to_tf.py \
---spanbert_config_path /xiaoya/pretrain_ckpt/spanbert_large_cased/config.json \
---bert_tf_ckpt_path /xiaoya/pretrain_ckpt/cased_L-24_H-1024_A-16/bert_model.ckpt \
---spanbert_pytorch_bin_path /xiaoya/pretrain_ckpt/spanbert_large_cased/pytorch_model.bin \
---output_spanbert_tf_dir /xiaoya/pretrain_ckpt/tf_spanbert_large_cased
+MODEL_NAME=$1
+PATH_TO_SPANBERT_PYTORCH_DIR=$2
+PATH_TO_SAME_SCALE_BERT_TF_DIR=$3
+PATH_TO_SAVE_SPANBERT_TF_DIR=$3
 
 
-
-# for spanbert base 
-echo "Transform SpanBERT Cased Large from Pytorch To TF"
-python3 ${REPO_PATH}/run/transform_spanbert_pytorch_to_tf.py \
---spanbert_config_path /xiaoya/pretrain_ckpt/spanbert_base_cased/config.json \
---bert_tf_ckpt_path /xiaoya/pretrain_ckpt/cased_L-12_H-768_A-12/bert_model.ckpt \
---spanbert_pytorch_bin_path /xiaoya/pretrain_ckpt/spanbert_base_cased/pytorch_model.bin \
---output_spanbert_tf_dir /xiaoya/pretrain_ckpt/tf_spanbert_base_cased
+if [[ $MODEL_NAME == "spanbert_base" ]]; then
+    # spanbert large 
+    echo "Transform SpanBERT Cased Base from Pytorch To TF"
+    python3 ${REPO_PATH}/run/transform_spanbert_pytorch_to_tf.py \
+        --spanbert_config_path $PATH_TO_SPANBERT_PYTORCH_DIR/config.json \
+        --bert_tf_ckpt_path $PATH_TO_SAME_SCALE_BERT_TF_DIR/bert_model.ckpt \
+        --spanbert_pytorch_bin_path $PATH_TO_SPANBERT_PYTORCH_DIR/pytorch_model.bin \
+        --output_spanbert_tf_dir $PATH_TO_SAVE_SPANBERT_TF_DIR
+elif [[ $MODEL_NAME == "spanbert_large" ]]; then
+    # spanbert base 
+    echo "Transform SpanBERT Cased Large from Pytorch To TF"
+    python3 ${REPO_PATH}/run/transform_spanbert_pytorch_to_tf.py \
+    --spanbert_config_path $PATH_TO_SPANBERT_PYTORCH_DIR/config.json \
+    --bert_tf_ckpt_path $PATH_TO_SAME_SCALE_BERT_TF_DIR/bert_model.ckpt \
+    --spanbert_pytorch_bin_path $PATH_TO_SPANBERT_PYTORCH_DIR/pytorch_model.bin \
+    --output_spanbert_tf_dir $PATH_TO_SAVE_SPANBERT_TF_DIR
+else
+    echo 'Unknown argment 1 (Model Sign)'
+fi 
